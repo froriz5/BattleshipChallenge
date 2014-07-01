9a8446018ba106800cb876cf0ae84cc8fc483e86
= <=>
2014-06-30 17:43:51 -0700

    Added my solutions

--- example/peopleco-challenge-nodejs_example.js
+++ example/peopleco-challenge-nodejs_example.js
@@ -1,33 +1,38 @@
 'use strict';
 
 var peoplecoChallengeNodejs = require('../lib/peopleco-challenge-nodejs.js'),
-    _und = require("../node_modules/underscore/underscore-min");
+    _und = require("underscore");
 
-var baseUrl = "student.ubuntudev.people.co",
-    yourUserHash = "04de00b15b33",
-    yourBaseUrl = "/api/challenge/battleship/"+yourUserHash+"/boards",
-    topAxis = ["A","B","C","D","E","F","G","H","I","J"],
-    leftAxis = ["1", "2","3","4","5","6","7","8","9","10"];
+var baseUrl = "student.people.co",
+    yourUserHash = "07fcb7ae4522",
+    yourBaseUrl = "/api/challenge/battleship/" + yourUserHash + "/boards/",
+    topAxis = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    leftAxis = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
-peoplecoChallengeNodejs.retrieveBoards(baseUrl, yourBaseUrl, function( boards ){
+peoplecoChallengeNodejs.retrieveBoards(baseUrl, yourBaseUrl, function(boards) {
 
     var boardsObjects = JSON.parse(boards);
-    peoplecoChallengeNodejs.retrieveBoard(boardsObjects[4], baseUrl, yourBaseUrl, function( boardResult ) {
+
+    peoplecoChallengeNodejs.retrieveBoard(boardsObjects[4], baseUrl, yourBaseUrl, function(boardResult) {
 
         console.log("Board is: ", boardResult);
 
 
-        for(var topAxi in topAxis)
-        {
+        for (var topAxi in topAxis) {
 
-            for ( var leftAxi in leftAxis )
-            {
+            for (var leftAxi in leftAxis) {
 
                 var coordinate = topAxis[topAxi] + leftAxis[leftAxi];
                 console.log("Coord: " + coordinate);
-                    peoplecoChallengeNodejs.makeMove(boardResult, coordinate, baseUrl, yourBaseUrl, function( moveResult ) {
-                        //handle result of move!
-                    });
+                peoplecoChallengeNodejs.makeMove(boardResult, coordinate, baseUrl, yourBaseUrl, function(moveResult) {
+                    //handle result of move!
+                    moveResult = JSON.parse("" + moveResult);
+                    if (!moveResult.is_hit) {
+                        peoplecoChallengeNodejs.makeMove(boardResult, coordinate, baseUrl, yourBaseUrl, function(result) {
+                            console.log("" + result);
+                        });
+                    }
+                });
 
 
             }
